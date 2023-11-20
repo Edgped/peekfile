a8877d3ff8127677b176493a14c67eb191fd83e6
Edgped <edgped@outlook.es>
2023-11-20 21:29:25 +0100

    Script edited in order to show the folder specified as the first argument in the output report

--- fastascan.sh
+++ fastascan.sh
@@ -1,6 +1,10 @@
 Fa_find=$(find . -name "*.fasta" -or -name "*.fa" | wc -l)
+if [[ -z $1 ]]; then Folder=$(echo "the current FOLDER "); else Folder=$(echo "the FOLDER '$1' ");fi
 		
 #REPORT
-echo 'In this FOLDER "" & SUBFOLDERS there is a total of $Fa_find fasta/fa files.'
 echo ''
-echo 'There are a total of Y unique fasta IDs, spread across the following files:'
+echo '## FASTA FILES REPORT ##'
+echo ''
+echo "In $Folder& its SUBFOLDERS there is a total of $Fa_find fasta/fa files."
+echo ''
+echo 'Spread across all that files, there are a total of Y unique fasta IDs'

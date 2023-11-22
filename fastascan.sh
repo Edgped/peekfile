eee405b60d83d28586afa149a39aa9ef9a3f8bbb
Edgped <edgped@outlook.es>
2023-11-22 17:37:29 +0100

    Added the block about the sequences inside each file

--- fastascan.sh
+++ fastascan.sh
@@ -1,9 +1,10 @@
 if [[ -z $1 ]]; then Folder=$(echo "the current FOLDER "); else Folder=$(echo "the FOLDER '$1' ");fi
-Fa_find=$(find . -name "*.fasta" -or -name "*.fa")
+Fa_find=$(find . -name "*.fasta" -or -name "*.fa" | grep -v "/\.") #Added a filter to avoid hidden files that generate errors in the output
 Fa_find_count=$(echo "$Fa_find" | wc -l)
-Fa_ids_count=$(grep -h ">" $Fa_find | sed 's/>//' | sort | uniq -c | wc -l)
+Fa_ids=$(grep -h ">" $Fa_find | sed 's/>//')
+Fa_ids_count=$(echo "$Fa_ids" | sort | uniq -c | wc -l)
 
-
+#echo "$Fa_ids"
 #REPORT
 echo ""
 echo "## FASTA FILES REPORT ##"
@@ -14,30 +15,62 @@ echo "Spread across all those files, there are a total of $Fa_ids_count unique f
 echo ""
 echo "FILE BREAKDOWN:"
 echo ""
-echo "--------------------------------------"
+echo "   -----------------------------------------"
 
 #Start of the loop to get a header for each file:
 file_counter=1
 for file in $Fa_find; do
-	
+	##File name:
 	echo "FILE $file_counter: $file"
 
-
-	#Symlink check:
+	##Symlink check:
 	if [[ -L "$file" ]]; then
 		echo "SYMLINK (Y/N): Yes"
 	else
 		echo "SYMLINK (Y/N): No"
 	fi
 	
+	##Number of seq.:
+	Seq_count=$(grep -c "^>" "$file")
 	
-	echo "IT CONTAINS X C SEQUENCE/S" #nucleotide or aminoacid (Put if nº is > than 1...) 
-	echo "TOTAL SEQUENCE LENGTH: D"
+	##Nucleotides or Aminoacids?
+	File_seqs=$(awk '/^>/{flag=1; next} flag{printf "%s", $0} END{print ""}' "$file")
+	#echo "$File_seqs" #To check.
+	
+	No_nucleotides=$(echo "$File_seqs" | sed '/>/! s/[-NATCGnatcg]//g' )
+	No_nucleotides_length=$(echo -n "$No_nucleotides" | wc -c)
+	#echo "$No_nucleotides, $No_nucleotides_length" #To check.
+	
+	if [[ "$No_nucleotides_length" -gt 1 ]]; then
+		Seq_type=$(echo "AMINOACIDIC")
+		Seq_unit=$(echo "AMINO ACIDS")
+	else
+		Seq_type=$(echo "NUCLEOTIDE")
+		Seq_unit=$(echo "NUCLEOTIDES")
+	fi
+		
+	
+	if [[ $Seq_count -gt 1 ]]; then
+		Seq_number=$(echo "SEQUENCES")
+	elif [[ $Seq_count -eq 1 ]]; then
+		Seq_number=$(echo "SEQUENCE")
+	else 
+		Seq_number=$(echo "SEQUENCES") #In order to get "0 SEQUENCES" in the case of an empty fasta file.
+	fi
+	
+	echo "THIS FILE CONTAINS $Seq_count $Seq_type $Seq_number" 
+	
+	##Seq. length:
+	File_seqs_length=$(echo -n "$File_seqs" | wc -c)
+	echo "TOTAL SEQUENCE LENGTH IN THE FILE: $File_seqs_length $Seq_unit"
+	
+	##File content:
 	echo ""
 	echo "CONTENT:"
 	echo "AAAAAAAAAAA"
+	
 	echo ""
-	echo "--------------------------------------"
+	echo "   -----------------------------------------"
 	((file_counter++))
 done
 echo ""
